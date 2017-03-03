aff3e1fcf3492c6e709d13affc2ec82943e5d71a
Nikita Bonachev <nbonachev@gmail.com>
2017-03-03 23:31:28 +0500

    init

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Root = () => (
+  <div>
+    <h1>Hello wallets!</h1>
+  </div>
+);
+
+ReactDOM.render(
+  <Root />,
+  document.getElementById('root')
+);
