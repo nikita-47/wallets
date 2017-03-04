eabefafaadd719b8358f53fb479ccbf19982b195
Nikita Bonachev <nbonachev@gmail.com>
2017-03-04 10:22:47 +0500

    add: load users

--- app/user-list/user-list.component.js
+++ app/user-list/user-list.component.js
@@ -4,39 +4,13 @@ angular.
   module('userList').
   component('userList', {
     templateUrl: 'user-list/user-list.template.html',
-    controller: function UserListController() {
-      this.users = [
-        {
-          "user_id": 0,
-          "user_name": "John",
-          "user_custom": "Smith",
-          "email": "email@example.com",
-          "register_date": "2014-12-08T21:44:32-08:00",
-          "balance": 100,
-          "wallet_amount": 1.5,
-          "wallet_currency": "USD",
-          "enabled": true
-        }, {
-          "user_id": 1,
-          "user_name": "Nick",
-          "user_custom": "Smith",
-          "email": "email1@example.com",
-          "register_date": "2014-05-08T21:44:32-08:00",
-          "balance": 100,
-          "wallet_amount": 1.5,
-          "wallet_currency": "USD",
-          "enabled": true
-        }, {
-          "user_id": 2,
-          "user_name": "Mary",
-          "user_custom": "Smith",
-          "email": "email2@example.com",
-          "register_date": "2014-01-30T21:44:32-08:00",
-          "balance": 100,
-          "wallet_amount": 1.5,
-          "wallet_currency": "USD",
-          "enabled": true
-        }
-      ];
-    }
+    controller: [
+      '$http',
+      function UserListController($http) {
+        var self = this;
+        $http.get('https://livedemo.xsolla.com/fe/test-task/baev/users?offset=0&limit=5').then(function (response) {
+            self.users = response.data.data;
+        });
+      }
+    ]
   });
